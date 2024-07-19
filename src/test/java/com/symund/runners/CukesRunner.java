3d2c28e5e524e3aa23af05a6b90a92956fcbafd3
ivangg75 <ivangg75@yahoo.com>
2024-07-19 16:55:16 -0400

    Tags in CukesRunner changed

--- src/test/java/com/symund/runners/CukesRunner.java
+++ src/test/java/com/symund/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/symund/step_definitions",
         dryRun = false,
-        tags = "@B34G8-196",
+        tags = "@B34G8-200",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
